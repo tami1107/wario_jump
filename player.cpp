e74a1434cf00295433a24fde6ddca74b97548ca8
tami <fko2247041@stu.o-hara.ac.jp>
2022-09-28 09:59:11 +0900

    player.cppのvoidPlayer::draw()を修正しました

--- player.cpp
+++ player.cpp
@@ -39,8 +39,21 @@ void Player::update()
 
 void Player::draw()
 {
-	if (m_isDead)	return;
-	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+	if (m_isDead)
+	{
+		DrawRectGraph(m_pos.x, m_pos.y,
+			m_graphSize.x / 2, 0, m_graphSize.x / 2, m_graphSize.y,
+			m_handle, TRUE, FALSE);
+	}
+	else
+	{
+		DrawRectGraph(m_pos.x, m_pos.y,
+			0, 0, m_graphSize.x/2, m_graphSize.y,
+			m_handle, TRUE, FALSE);
+
+	}
+	//DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+
 }
 
 bool Player::isCol(Car& car)
