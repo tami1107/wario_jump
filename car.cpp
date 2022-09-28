7c914a944bc050e549e916cfdd82f4e438285a24
tami <fko2247041@stu.o-hara.ac.jp>
2022-09-28 11:38:51 +0900

    ジャンプ力および車が出てくるまでの時間を設定ができるように編集を行った

--- car.cpp
+++ car.cpp
@@ -2,8 +2,19 @@
 #include "game.h"
 #include "car.h"
 
+namespace
+{
+	// 待ち時間
+	constexpr int kWaitFrameMin = 60;
+	constexpr int kWaitFrameMax = 180;
+	// 車の速度
+	constexpr float kSpeed = -20.0f;
+}
+
 Car::Car()
 {
+//	GetRand(100);//0~100
+
 	m_handle = -1;
 	m_fieldY = 0.0f;
 }
@@ -20,16 +31,25 @@ void Car::setup(float fieldY)
 	m_pos.x = Game::kScreenWidth + 16.0f;
 	m_pos.y = m_fieldY - m_size.y;
 
-	m_vec.x = -16.0f;
+	m_vec.x = kSpeed;
 	m_vec.y = 0.0f;
+
+	// 動き始めるまでの時間を設定	1秒から3秒待つ	60フレームから180フレーム
+	m_waitFrame = GetRand(kWaitFrameMax - kWaitFrameMin) + kWaitFrameMin;
 }
 
 void Car::update()
 {
+	if (m_waitFrame > 0)
+	{
+		m_waitFrame--;
+		return;
+	}
 	m_pos += m_vec;
 }
 
 void Car::draw()
 {
 	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+	//DrawFormatString(0,0,GetColor(255,255,255),"wait:%d")
 }
